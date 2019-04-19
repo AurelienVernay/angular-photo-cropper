6521406fa69a019bac402a50b76325f0f01a8e31
Aurélien VERNAY <aurevernay@yahoo.fr>
2019-04-19 19:44:55 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'photo-cropper';
+    public pictureLoaded = false;
+    public filename = 'Untitled picture';
+    loadPicture() {
+        this.pictureLoaded = false;
+        document.getElementById('fileInput').click();
+    }
+
+    onFileUploaded(event) {
+        const file = event.srcElement.files.item(0);
+        const canvas = <HTMLCanvasElement>(
+            document.getElementById('picture-canvas')
+        );
+        const mainContainer = document.getElementById('main-container');
+        const context = canvas.getContext('2d');
+        if (file) {
+            const FR = new FileReader();
+            FR.onload = e => {
+                var img = new Image();
+                img.addEventListener('load', () => {
+                    let ratio = 1;
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+                    if (
+                        img.width > mainContainer.clientWidth ||
+                        img.height > mainContainer.clientHeight
+                    ) {
+                        const heightRatio =
+                            mainContainer.clientHeight / img.height;
+                        const widthRatio =
+                            (mainContainer.clientWidth - 20) / img.width;
+                        ratio =
+                            heightRatio < widthRatio ? heightRatio : widthRatio;
+                        canvas.width = img.width * ratio;
+                        canvas.height = img.height * ratio;
+                    }
+                    context.scale(ratio, ratio);
+                    context.drawImage(img, 0, 0);
+                    this.pictureLoaded = true;
+                });
+                const target: any = e.target;
+                img.src = target.result;
+            };
+            FR.readAsDataURL(file);
+        }
+    }
+
+    public saveFile() {
+        const anchor = document.getElementById('download-anchor');
+        const image = document.getElementById(
+            'picture-canvas'
+        ) as HTMLCanvasElement;
+        anchor.setAttribute('href', image.toDataURL('image/png'));
+        anchor.click();
+    }
+
+    public rotateLeft() {
+        this.rotate(-90);
+    }
+    public rotateRight() {
+        this.rotate(90);
+    }
+    private rotate(angle: number) {
+        const canvas = document.getElementById(
+            'picture-canvas'
+        ) as HTMLCanvasElement;
+        const ctx = canvas.getContext('2d');
+        const dataURL = canvas.toDataURL();
+        const image = new Image();
+        image.addEventListener('load', () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.translate(image.width, image.height);
+            // context.rotate(180 * (Math.PI / 180));
+            ctx.rotate(angle);
+            ctx.drawImage(image, 0, 0);
+            const tmp = canvas.width;
+            canvas.width = canvas.height;
+            canvas.height = tmp;
+        });
+        image.src = dataURL;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+const matModules = [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTooltipModule,
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        FlexLayoutModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        matModules,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
